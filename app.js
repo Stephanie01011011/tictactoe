c4ef93c99522b56d8aba081215fea9ea5d394e6c
Stephanie Livengood <sreneelive@gmail.com>
2023-04-16 17:38:26 -0400

    Create html css and JS files. Create simple style of page.

---
+++ app.js
@@ -0,0 +1,18 @@
+const gameBoard = (() => {
+    let board = [0,0,0,
+                 0,0,0,
+                 0,0,0]
+    return {
+        board
+    }
+})();
+
+const displayController = (() => {
+
+    return {
+        
+    }
+})
+const Player = () => {
+
+}
