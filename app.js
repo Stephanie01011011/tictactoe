e87c1de668dc5eda2e45a6061cf2a51e4472602a
Stephanie Livengood <sreneelive@gmail.com>
2023-04-17 18:23:40 -0400

    Can find a winner, and personalized messages. Players can place a mark anywhere they want.

--- app.js
+++ app.js
@@ -1,43 +1,181 @@
-let count = 1;
 
 const gameBoard = (() => {
     let board = ["","","",
                  "","","",
                  "","",""];
     
+    
+
     return {
         board
     };
 })();
 
 const displayController = (() => {
+    let mark = "O";
+    let winner = false;
     
-    //Display the gameboard everytime a user places a mark
+    //Push mark to array for display   
+    let getMark = () => {
+        if(winner == true){
+            clearBoard();
+        }
+      let player = document.getElementById("player");
+          if (mark == "X"){
+            
+            mark = "O";
+            player.textContent = player1.name + ", make your move";
+            
+        } else {
+            mark = "X";
+            player.textContent = player2.name + ", make your move";
+            
+        }
+
+        if(event.target.className == "0"){
+            gameBoard.board[0] = mark;
+            displayBoard();
+            checkForWinner();
+        } else if(event.target.className == "1"){
+            gameBoard.board[1] = mark;
+            displayBoard();
+            checkForWinner();
+        }else if(event.target.className == "2"){
+            gameBoard.board[2] = mark;
+            displayBoard();
+            checkForWinner();
+        } else if(event.target.className == "3"){
+            gameBoard.board[3] = mark;
+            displayBoard();
+            checkForWinner();
+        } else if(event.target.className == "4"){
+            gameBoard.board[4] = mark;
+            displayBoard();
+            checkForWinner();
+        } else if(event.target.className == "5"){
+            gameBoard.board[5] = mark;
+            displayBoard();
+            checkForWinner();
+        } else if(event.target.className == "6"){
+            gameBoard.board[6] = mark;
+            displayBoard();
+            checkForWinner();
+        } else if(event.target.className == "7"){
+            gameBoard.board[7] = mark;
+            displayBoard();
+            checkForWinner();
+        } else if(event.target.className == "8"){
+            gameBoard.board[8] = mark;
+            displayBoard();
+            checkForWinner();
+        }
+
+       
+       
+        //check for a winner after each move
+        function checkForWinner(){
+            
+            if(gameBoard.board[0] == gameBoard.board[1] && gameBoard.board[1]== gameBoard.board[2] ){
+                if (gameBoard.board[0] != ""){
+                    alert("There is a winner");
+                    winner = true;
+                }
+                
+            } else if(gameBoard.board[3] == gameBoard.board[4] && gameBoard.board[4]== gameBoard.board[5] ){
+                if (gameBoard.board[3] != ""){
+                    alert("There is a winner");
+                    winner = true;
+                }
+            } else if(gameBoard.board[6] == gameBoard.board[7] && gameBoard.board[7]== gameBoard.board[8] ){
+                if (gameBoard.board[6] != ""){
+                    alert("There is a winner");
+                    winner = true;
+                }
+            } else if(gameBoard.board[0] == gameBoard.board[3] && gameBoard.board[3]== gameBoard.board[6] ){
+                if (gameBoard.board[0] != ""){
+                    alert("There is a winner");
+                    winner = true;
+                }
+            } else if(gameBoard.board[1] == gameBoard.board[4] && gameBoard.board[4]== gameBoard.board[7] ){
+                if (gameBoard.board[1] != ""){
+                    alert("There is a winner");
+                    winner = true;
+                }
+            } else if(gameBoard.board[2] == gameBoard.board[5] && gameBoard.board[5]== gameBoard.board[8] ){
+                if (gameBoard.board[2] != ""){
+                    alert("There is a winner");
+                    winner = true;
+                }
+            } else if(gameBoard.board[0] == gameBoard.board[4] && gameBoard.board[4]== gameBoard.board[8] ){
+                if (gameBoard.board[0] != ""){
+                    alert("There is a winner");
+                    winner = true;
+                }
+            } else if(gameBoard.board[2] == gameBoard.board[4] && gameBoard.board[4]== gameBoard.board[6] ){
+                if (gameBoard.board[2] != ""){
+                    alert("There is a winner");
+                    if(mark == "X"){
+                        player.textContent = player1.name + " is the winner!";
+                    } else {
+                        player.textContent = player2.name + " is the winner!";
+                    }
+                    winner = true;
+                }
+            }
+        }
+        function clearBoard() {
+            gameBoard.board = ["","","","","","","","",""];
+            winner = false;
+            mark = "O";
+            player.textContent = player1.name + ", make your move";
+            displayBoard();
+        }
+
+
+
+  
+   }
+   
+    
+       //Display the gameboard everytime a user places a mark
     let displayBoard = () => {
+        
         let topLeft = document.getElementById("top-left");
-        topLeft.textContent = gameBoard.board[0];
         let topCenter = document.getElementById("top-center");
-        topCenter.textContent = gameBoard.board[1];
         let topRight = document.getElementById("top-right");
-        topRight.textContent = gameBoard.board[2];
         let centerLeft = document.getElementById("center-left");
-        centerLeft.textContent = gameBoard.board[3];
         let centerCenter = document.getElementById("center-center");
-        centerCenter.textContent = gameBoard.board[4];
         let centerRight = document.getElementById("center-right");
-        centerRight.textContent = gameBoard.board[5];
         let bottomLeft = document.getElementById("bottom-left");
-        bottomLeft.textContent = gameBoard.board[6];
         let bottomCenter = document.getElementById("bottom-center");
-        bottomCenter.textContent = gameBoard.board[7];
         let bottomRight = document.getElementById("bottom-right");
+        topLeft.textContent = gameBoard.board[0];
+        topCenter.textContent = gameBoard.board[1];
+        topRight.textContent = gameBoard.board[2];
+        centerLeft.textContent = gameBoard.board[3];
+        centerCenter.textContent = gameBoard.board[4];
+        centerRight.textContent = gameBoard.board[5];
+        bottomLeft.textContent = gameBoard.board[6];
+        bottomCenter.textContent = gameBoard.board[7];
         bottomRight.textContent = gameBoard.board[8];
+        
          }
+
+
+ 
     return {
-        displayBoard
+        getMark, displayBoard
     };
 })();
 
-const Player = () => {
-
+const Player = (name, mark) => {
+    return {
+        name, mark
+    };
 }
+
+const player1 = Player("Stephanie", "X");
+const player2 = Player("Alexander", "O");
+
+
+
