f9e883435f413f1eb038f61961fd4bdc4d2e1947
Stephanie Livengood <sreneelive@gmail.com>
2023-04-17 13:59:59 -0400

    Added display board functionality.

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
 let count = 1;
 
 const gameBoard = (() => {
-    let board = ["x","x","x",
-                 "x","x","x",
-                 "x","x","x"];
-    let topLeft = document.getElementById("top-left");
-    let displayBoard = () => {
-        let topLeft = document.getElementById("top-left");
-        topLeft.textContent = board[0];
-        console.log("hi");
-        return topLeft;
-         }
+    let board = ["","","",
+                 "","","",
+                 "","",""];
+    
     return {
-        board, displayBoard
+        board
     };
 })();
 
 const displayController = (() => {
-
+    
+    //Display the gameboard everytime a user places a mark
+    let displayBoard = () => {
+        let topLeft = document.getElementById("top-left");
+        topLeft.textContent = gameBoard.board[0];
+        let topCenter = document.getElementById("top-center");
+        topCenter.textContent = gameBoard.board[1];
+        let topRight = document.getElementById("top-right");
+        topRight.textContent = gameBoard.board[2];
+        let centerLeft = document.getElementById("center-left");
+        centerLeft.textContent = gameBoard.board[3];
+        let centerCenter = document.getElementById("center-center");
+        centerCenter.textContent = gameBoard.board[4];
+        let centerRight = document.getElementById("center-right");
+        centerRight.textContent = gameBoard.board[5];
+        let bottomLeft = document.getElementById("bottom-left");
+        bottomLeft.textContent = gameBoard.board[6];
+        let bottomCenter = document.getElementById("bottom-center");
+        bottomCenter.textContent = gameBoard.board[7];
+        let bottomRight = document.getElementById("bottom-right");
+        bottomRight.textContent = gameBoard.board[8];
+         }
     return {
+        displayBoard
+    };
+})();
 
-    }
-})
 const Player = () => {
 
 }
